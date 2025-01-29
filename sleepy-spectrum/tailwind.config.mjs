807ac180d85ea769e2f68513498eda250cbe81ac
rxzkie <54205705+rxzkie@users.noreply.github.com>
2025-01-29 09:48:30 -0300

    fist
    commit

---
+++ sleepy-spectrum/tailwind.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	darkMode: 'class', // Habilita el modo oscuro basado en clases
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+	  extend: {},
+	},
+	plugins: [],
+  }
+  
